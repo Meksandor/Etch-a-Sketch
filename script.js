21e56a1b6f53097f029be068f3901a1cada422bc
Meksandor <526473+gb96@users.noreply.github.com>
2023-10-29 21:20:39 +0300

    Complete ratio buttons work and div creation inside of container according to the clicked ratio button

---
+++ script.js
@@ -0,0 +1,120 @@
+let divsContainer = document.querySelector('#container');
+var ratioButtons = document.querySelector('.ratioButton')
+
+/*
+ratioButtons.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        var targetRatioButton = event.target;
+        switch (targetRatioButton.id) {
+            case 'ratio16':
+                return i = 16;
+
+            case 'ratio25':
+                return i = 25;
+
+            case 'ratio36':
+                return i = 36;
+
+            case 'ratio49':
+                return i = 49;
+
+            case 'ratio64':
+                return i = 64;
+
+            case 'ratio81':
+                return i = 81;
+
+            case 'ratio100':
+                return i = 100;
+        }
+    })
+})
+*/
+
+var button16 = document.getElementById("ratio16");
+var button25 = document.getElementById("ratio25");
+var button36 = document.getElementById("ratio36");
+var button49 = document.getElementById("ratio49");
+var button64 = document.getElementById("ratio64");
+var button81 = document.getElementById("ratio81");
+var button100 = document.getElementById("ratio100");
+
+
+button16.addEventListener("click", function () {
+    for (var i = 0; i < 16; i++) {
+        var div = document.createElement("div");
+        div.id = "Div " + (i + 1);
+        divsContainer.appendChild(div);
+    }
+    const gridBoxes = document.querySelectorAll('#container > div');
+    gridBoxes.forEach((box) => {
+        box.setAttribute('class', `gridBoxPiece`);
+    });
+});
+button25.addEventListener("click", function () {
+    for (var i = 0; i < 25; i++) {
+        var div = document.createElement("div");
+        div.id = "Div " + (i + 1);
+        divsContainer.appendChild(div);
+    }
+    const gridBoxes = document.querySelectorAll('#container > div');
+    gridBoxes.forEach((box) => {
+        box.setAttribute('class', `gridBoxPiece`);
+    });
+});
+button36.addEventListener("click", function () {
+    for (var i = 0; i < 36; i++) {
+        var div = document.createElement("div");
+        div.id = "Div " + (i + 1);
+        divsContainer.appendChild(div);
+    }
+    const gridBoxes = document.querySelectorAll('#container > div');
+    gridBoxes.forEach((box) => {
+        box.setAttribute('class', `gridBoxPiece`);
+    });
+});
+button49.addEventListener("click", function () {
+    for (var i = 0; i < 49; i++) {
+        var div = document.createElement("div");
+        div.id = "Div " + (i + 1);
+        divsContainer.appendChild(div);
+    }
+    const gridBoxes = document.querySelectorAll('#container > div');
+    gridBoxes.forEach((box) => {
+        box.setAttribute('class', `gridBoxPiece`);
+    });
+});
+button64.addEventListener("click", function () {
+    for (var i = 0; i < 64; i++) {
+        var div = document.createElement("div");
+        div.id = "Div " + (i + 1);
+        divsContainer.appendChild(div);
+    }
+    const gridBoxes = document.querySelectorAll('#container > div');
+    gridBoxes.forEach((box) => {
+        box.setAttribute('class', `gridBoxPiece`);
+    });
+});
+button81.addEventListener("click", function () {
+    for (var i = 0; i < 81; i++) {
+        var div = document.createElement("div");
+        div.id = "Div " + (i + 1);
+        divsContainer.appendChild(div);
+    }
+    const gridBoxes = document.querySelectorAll('#container > div');
+    gridBoxes.forEach((box) => {
+        box.setAttribute('class', `gridBoxPiece`);
+    });
+});
+button100.addEventListener("click", function () {
+    for (var i = 0; i < 100; i++) {
+        var div = document.createElement("div");
+        div.id = "Div " + (i + 1);
+        divsContainer.appendChild(div);
+    }
+    const gridBoxes = document.querySelectorAll('#container > div');
+    gridBoxes.forEach((box) => {
+        box.setAttribute('class', `gridBoxPiece`);
+    });
+});
+
