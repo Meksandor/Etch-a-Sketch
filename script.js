f1a9baa1ab7b4473cc75caa9d74f4ea5e04a353f
Meksandor <526473+gb96@users.noreply.github.com>
2023-11-02 17:06:55 +0300

    Add gif and improve rainbow toggle button functionality

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// script.js
 const container = document.querySelector('#container');
 const containerDivSquares = document.querySelectorAll('.containerDivSquare');
 const slider = document.querySelector('#sliderBar');
 const clearButton = document.querySelector('#clearButton')
+const toggleButton = document.querySelector("#switch-shadow");
 
 function createGridAndPaint(rows, cols) {
     container.innerHTML = "";
@@ -28,6 +28,7 @@ slider.addEventListener("input", () => {
     const value = slider.value;
     createGridAndPaint(value, value);
 });
+
 // Initial grid creation
 createGridAndPaint(14, 14);
 
@@ -37,3 +38,40 @@ clearButton.addEventListener('click', function () {
         squareElement.style.backgroundColor = "lightgray";
     })
 });
+
+toggleButton.addEventListener("change", function () {
+    var body = document.body;
+    var backgroundImageGif = new Image();
+    backgroundImageGif.src = 'media/undertale.gif';
+
+    var optionTexts = document.querySelectorAll('.optionText')
+
+    if (toggleButton.checked) {
+        backgroundImageGif.onload = function () {
+            // Set the background image using the style property
+            body.style.backgroundImage = 'url(' + backgroundImageGif.src + ')';
+            body.style.backgroundSize = 'cover'; // You can adjust this property as needed
+
+            optionTexts.forEach(function (text) {
+                text.style.color = 'white'
+            });
+
+            // Optionally, set other background properties like background repeat, position, etc.
+            // body.style.backgroundRepeat = 'no-repeat';
+            // body.style.backgroundPosition = 'center';
+            // BackgroundPosition to center, etc. as needed
+        };
+
+        function rainbowHoverContainerSquares() {
+
+        }
+    }
+    else {
+        body.style.backgroundImage = 'none';
+        body.style.backgroundColor = 'white';
+        optionTexts.forEach(function (text) {
+            text.style.color = 'black'
+        });
+    }
+});
+
