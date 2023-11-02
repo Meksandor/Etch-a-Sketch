73129c2dda531bdeefd11a0ba2da99d7ba2ee91e
Meksandor <526473+gb96@users.noreply.github.com>
2023-11-02 15:11:48 +0300

    Create painting and improve functions
    Improved createGrid function to be able to print divs inside of container.

--- script.js
+++ script.js
@@ -2,35 +2,29 @@
 const container = document.querySelector('#container');
 const slider = document.querySelector('#sliderBar');
 
-function createGrid(rows, cols) {
+function createGridAndPaint(rows, cols) {
     container.innerHTML = "";
     container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
 
     for (let i = 0; i < rows * cols; i++) {
         const square = document.createElement("div");
-        square.classList.add("square");
+        square.classList.add("containerDivSquare");
         container.appendChild(square);
     }
-}
 
+    var containerDivSquares = document.querySelectorAll('.containerDivSquare');
+    containerDivSquares.forEach(function (squareElement) {
+        squareElement.addEventListener('mouseover', function () {
+            console.log('yehha')
+            squareElement.style.backgroundColor = "orange";
+        });
+    });
+}
 
 slider.addEventListener("input", () => {
     const value = slider.value;
-    createGrid(value, value);
+    createGridAndPaint(value, value);
 });
-
-
-/*
-function getSliderBarValue() {
-    slider.addEventListener('input', function () {
-        var sliderBarValue = slider.value;
-        // console.log(sliderBarValue)
-        return sliderBarValue;
-    });
-}
-createGrid(getSliderBarValue(), getSliderBarValue())
-*/
-
 // Initial grid creation
-createGrid(14, 14);
+createGridAndPaint(14, 14);
